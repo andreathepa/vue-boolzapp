899c72fbd1de09e198700fd0df25a5d1d4346b08
andreathepa <andreadepa32@gmail.com>
2023-06-14 10:00:15 +0200

    STYLE

--- js/script.js
+++ js/script.js
@@ -167,14 +167,72 @@ createApp({
                       status: 'received'
                   }
               ],
-          }
+          },
+          {
+            name: 'Alessandro L.',
+            avatar: './img/avatar_5.jpg',
+            visible: true,
+            messages: [
+                {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Ricordati di chiamare la nonna',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Va bene, stasera la sento',
+                    status: 'received'
+                }
+            ],
+        },
+        {
+            name: 'Giovanni',
+            avatar: './img/avatar_5.jpg',
+            visible: true,
+            messages: [
+                {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Ciao Claudia, hai novità?',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Non ancora',
+                    status: 'received'
+                },
+                {
+                    date: '10/01/2020 15:51:00',
+                    message: 'Nessuna nuova, buona nuova',
+                    status: 'sent'
+                }
+            ],
+        },
+        {
+            name: 'Aldo',
+            avatar: './img/avatar_3.jpg',
+            visible: true,
+            messages: [
+                {
+                    date: '28/03/2020 10:10:40',
+                    message: 'La Marianna va in campagna',
+                    status: 'received'
+                },
+                {
+                    date: '28/03/2020 10:20:10',
+                    message: 'Sicuro di non aver sbagliato chat?',
+                    status: 'sent'
+                },
+                {
+                    date: '28/03/2020 16:15:22',
+                    message: 'Ah scusa!',
+                    status: 'received'
+                }
+            ],
+        }
       ]
-      
-        
-
-
 
       }
+
     },
     methods: {
       changeChat(index){
